c23930d4a9e1fca6d903d5a5b85ed3cfe79b06ed
Rune Holmgren <Raane.Holm@gmail.com>
2014-08-31 09:20:57 +0200

    mpi running on rune.es

--- ps1/mpi.c
+++ ps1/mpi.c
@@ -1,7 +1,21 @@
-#include <mpi.h>
 #include <stdio.h>
+#include <mpi.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv) {
-
+int main(int argc, char** argv){
+  int size, rank, in;
+  MPI_Status status;
+  MPI_Init(&argc,&argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if(rank == 0){
+    for(int i = 1; i < size; i++){
+      MPI_Send(&i, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+    }
+  }
+  else{
+    MPI_Recv(&in, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+    printf("Rank %d recieved %d\n",rank, in);
+  }
+  MPI_Finalize();
 }
