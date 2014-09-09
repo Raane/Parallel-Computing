0dc558aba840eea10d9e5544856cc811b0367da4
Rune Holmgren <Raane.Holm@gmail.com>
2014-09-09 19:28:20 +0200

    Sort of done distribute image function

--- ps2/region.c
+++ ps2/region.c
@@ -5,14 +5,14 @@
 #include "bmp.h"
 
 typedef struct{
-    int x;
-    int y;
+  int x;
+  int y;
 } pixel_t;
 
 typedef struct{
-    int size;
-    int buffer_size;
-    pixel_t* pixels;
+  int size;
+  int buffer_size;
+  pixel_t* pixels;
 } stack_t;
 
 
@@ -43,190 +43,195 @@ unsigned char *image,           // Entire image, only on rank 0
 
 // Create new pixel stack
 stack_t* new_stack(){
-    stack_t* stack = (stack_t*)malloc(sizeof(stack_t));
-    stack->size = 0;
-    stack->buffer_size = 1024;
-    stack->pixels = (pixel_t*)malloc(sizeof(pixel_t*)*1024);
+  stack_t* stack = (stack_t*)malloc(sizeof(stack_t));
+  stack->size = 0;
+  stack->buffer_size = 1024;
+  stack->pixels = (pixel_t*)malloc(sizeof(pixel_t*)*1024);
 }
 
 
 // Push on pixel stack
 void push(stack_t* stack, pixel_t p){
-    if(stack->size == stack->buffer_size){
-        stack->buffer_size *= 2;
-        stack->pixels = realloc(stack->pixels, sizeof(pixel_t)*stack->buffer_size);
-    }
-    stack->pixels[stack->size] = p;
-    stack->size += 1;
+  if(stack->size == stack->buffer_size){
+    stack->buffer_size *= 2;
+    stack->pixels = realloc(stack->pixels, sizeof(pixel_t)*stack->buffer_size);
+  }
+  stack->pixels[stack->size] = p;
+  stack->size += 1;
 }
 
 
 // Pop from pixel stack
 pixel_t pop(stack_t* stack){
-    stack->size -= 1;
-    return stack->pixels[stack->size];
+  stack->size -= 1;
+  return stack->pixels[stack->size];
 }
 
 
 // Check if two pixels are similar. The hardcoded threshold can be changed.
 // More advanced similarity checks could have been used.
 int similar(unsigned char* im, pixel_t p, pixel_t q){
-    int a = im[p.x +  p.y * image_size[1]];
-    int b = im[q.x +  q.y * image_size[1]];
-    int diff = abs(a-b);
-    return diff < 2;
+  int a = im[p.x +  p.y * image_size[1]];
+  int b = im[q.x +  q.y * image_size[1]];
+  int diff = abs(a-b);
+  return diff < 2;
 }
 
 
 // Create and commit MPI datatypes
 void create_types(){
-    
+
 }
 
 
 // Send image from rank 0 to all ranks, from image to local_image
 void distribute_image(){
-    
-    
+  for(int dest=1;dest<size;dest++) {
+    for(int row=0;row<image_size[1];row++) {
+      MPI_Send(
+          (image + (coords[1] * local_image_size[1] + row) * image_size[0] + coords[0] * local_image_size[0]) -1, 
+          local_image_size[0] + 2, MPI_UNSIGNED_CHAR, dest, 0, MPI_COMM_WORLD);
+    }
+  }
 }
 
 
 // Exchange borders with neighbour ranks
 void exchange(stack_t* stack){
-    
-    
+
+
 }
 
 
 // Gather region bitmap from all ranks to rank 0, from local_region to region
 void gather_region(){
-    
-    
+
+
 }
 
 // Determine if all ranks are finished. You may have to add arguments.
 // You dont have to have this check as a seperate function
 int finished(){
-   
+
 }
 
 
 // Check if pixel is inside local image
 int inside(pixel_t p){
-    return (p.x >= 0 && p.x < local_image_size[1] && p.y >= 0 && p.y < local_image_size[0]);
+  return (p.x >= 0 && p.x < local_image_size[1] && p.y >= 0 && p.y < local_image_size[0]);
 }
 
 
 // Adding seeds in corners.
 void add_seeds(stack_t* stack){
-    int seeds [8];
-    seeds[0] = 5;
-    seeds[1] = 5;
-    seeds[2] = image_size[1]-5;
-    seeds[3] = 5;
-    seeds[4] = image_size[1]-5;
-    seeds[5] = image_size[0]-5;
-    seeds[6] = 5;
-    seeds[7] = image_size[0]-5;
-    
-    for(int i = 0; i < 4; i++){
-        pixel_t seed;
-        seed.x = seeds[i*2] - coords[1]*local_image_size[1];
-        seed.y = seeds[i*2+1] -coords[0]*local_image_size[0];
-        
-        if(inside(seed)){
-            push(stack, seed);
-        }
+  int seeds [8];
+  seeds[0] = 5;
+  seeds[1] = 5;
+  seeds[2] = image_size[1]-5;
+  seeds[3] = 5;
+  seeds[4] = image_size[1]-5;
+  seeds[5] = image_size[0]-5;
+  seeds[6] = 5;
+  seeds[7] = image_size[0]-5;
+
+  for(int i = 0; i < 4; i++){
+    pixel_t seed;
+    seed.x = seeds[i*2] - coords[1]*local_image_size[1];
+    seed.y = seeds[i*2+1] -coords[0]*local_image_size[0];
+
+    if(inside(seed)){
+      push(stack, seed);
     }
+  }
 }
 
 
 // Region growing, serial implementation
 void grow_region(){
-    
-    stack_t* stack = new_stack();
-    add_seeds(stack);
-        
-    while(stack->size > 0){
-        pixel_t pixel = pop(stack);
-        
-        region[pixel.y * image_size[1] + pixel.x] = 1;
-        
-        
-        int dx[4] = {0,0,1,-1}, dy[4] = {1,-1,0,0};
-        for(int c = 0; c < 4; c++){
-            pixel_t candidate;
-            candidate.x = pixel.x + dx[c];
-            candidate.y = pixel.y + dy[c];
-            
-            if(!inside(candidate)){
-                continue;
-            }
-            
-            
-            if(region[candidate.y * image_size[1] + candidate.x]){
-                continue;
-            }
-            
-            if(similar(image, pixel, candidate)){
-                region[candidate.x + candidate.y * image_size[1]] = 1;
-                push(stack,candidate);
-            }
-        }
+
+  stack_t* stack = new_stack();
+  add_seeds(stack);
+
+  while(stack->size > 0){
+    pixel_t pixel = pop(stack);
+
+    region[pixel.y * image_size[1] + pixel.x] = 1;
+
+
+    int dx[4] = {0,0,1,-1}, dy[4] = {1,-1,0,0};
+    for(int c = 0; c < 4; c++){
+      pixel_t candidate;
+      candidate.x = pixel.x + dx[c];
+      candidate.y = pixel.y + dy[c];
+
+      if(!inside(candidate)){
+        continue;
+      }
+
+
+      if(region[candidate.y * image_size[1] + candidate.x]){
+        continue;
+      }
+
+      if(similar(image, pixel, candidate)){
+        region[candidate.x + candidate.y * image_size[1]] = 1;
+        push(stack,candidate);
+      }
     }
+  }
 }
 
 
 // MPI initialization, setting up cartesian communicator
 void init_mpi(int argc, char** argv){
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
-    MPI_Dims_create(size, 2, dims);
-    MPI_Cart_create( MPI_COMM_WORLD, 2, dims, periods, 0, &cart_comm );
-    MPI_Cart_coords( cart_comm, rank, 2, coords );
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    printf("dim 0: %d, ", dims[0]);
-    printf("dim 1: %d, ", dims[0]);
-    printf("coor 0: %d, ", coords[0]);
-    printf("coor 1: %d\n", coords[1]);
-  
-    MPI_Cart_shift( cart_comm, 0, 1, &north, &south );
-    MPI_Cart_shift( cart_comm, 1, 1, &west, &east );
+  MPI_Dims_create(size, 2, dims);
+  MPI_Cart_create( MPI_COMM_WORLD, 2, dims, periods, 0, &cart_comm );
+  MPI_Cart_coords( cart_comm, rank, 2, coords );
+
+  printf("dim 0: %d, ", dims[0]);
+  printf("dim 1: %d, ", dims[0]);
+  printf("coor 0: %d, ", coords[0]);
+  printf("coor 1: %d\n", coords[1]);
+
+  MPI_Cart_shift( cart_comm, 0, 1, &north, &south );
+  MPI_Cart_shift( cart_comm, 1, 1, &west, &east );
 }
 
 
 void load_and_allocate_images(int argc, char** argv){
 
-    if(argc != 2){
-        printf("Useage: region file");
-        exit(-1);
-    }
-    
-    if(rank == 0){
-        image = read_bmp(argv[1]);
-        region = (unsigned char*)calloc(sizeof(unsigned char),image_size[0]*image_size[1]);
-    }
-    
-    local_image_size[0] = image_size[0]/dims[0];
-    local_image_size[1] = image_size[1]/dims[1];
-    
-    int lsize = local_image_size[0]*local_image_size[1];
-    int lsize_border = (local_image_size[0] + 2)*(local_image_size[1] + 2);
-    local_image = (unsigned char*)malloc(sizeof(unsigned char)*lsize_border);
-    local_region = (unsigned char*)calloc(sizeof(unsigned char),lsize_border);
+  if(argc != 2){
+    printf("Useage: region file");
+    exit(-1);
+  }
+
+  if(rank == 0){
+    image = read_bmp(argv[1]);
+    region = (unsigned char*)calloc(sizeof(unsigned char),image_size[0]*image_size[1]);
+  }
+
+  local_image_size[0] = image_size[0]/dims[0];
+  local_image_size[1] = image_size[1]/dims[1];
+
+  int lsize = local_image_size[0]*local_image_size[1];
+  int lsize_border = (local_image_size[0] + 2)*(local_image_size[1] + 2);
+  local_image = (unsigned char*)malloc(sizeof(unsigned char)*lsize_border);
+  local_region = (unsigned char*)calloc(sizeof(unsigned char),lsize_border);
 }
 
 
 void write_image(){
-    if(rank==0){
-        for(int i = 0; i < image_size[0]*image_size[1]; i++){
+  if(rank==0){
+    for(int i = 0; i < image_size[0]*image_size[1]; i++){
 
-            image[i] *= (region[i] == 0);
-        }
-        write_bmp(image, image_size[0], image_size[1]);
+      image[i] *= (region[i] == 0);
     }
+    write_bmp(image, image_size[0], image_size[1]);
+  }
 }
 
 
@@ -252,6 +257,7 @@ int main(int argc, char** argv){
     printf("Rank %d started!\n", rank);
     MPI_Finalize();
   }
+  //  printf("rank(%d): n,s,e,w: %d %d %d %d\n", rank, north, south, east, west);
 
   exit(0);
 }
