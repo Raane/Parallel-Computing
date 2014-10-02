1e1d6f98f630ec684c85ff1c2e4b5562aa71dfc7
Rune Holmgren <Raane.Holm@gmail.com>
2014-10-02 11:56:07 +0200

    Add code to test that everything is ok

--- ps4/histogram_omp.c
+++ ps4/histogram_omp.c
@@ -10,35 +10,46 @@ const int color_depth = 255;
 
 int main(int argc, char** argv){
 
-    if(argc != 3){
-        printf("Useage: %s image n_threads\n", argv[0]);
-        exit(-1);
+  if(argc != 3){
+    printf("Useage: %s image n_threads\n", argv[0]);
+    exit(-1);
+  }
+  int n_threads = atoi(argv[2]);
+
+  unsigned char* image = read_bmp(argv[1]);
+  unsigned char* fasit = read_bmp("fasit.bmp");
+  unsigned char* output_image = malloc(sizeof(unsigned char) * image_size);
+
+
+  int* histogram = (int*)calloc(sizeof(int), color_depth);
+  #pragma omp parallel for
+  for(int i = 0; i < image_size; i++){
+    histogram[image[i]]++;
+  }
+
+
+  float* transfer_function = (float*)calloc(sizeof(float), color_depth);
+  #pragma omp parallel for
+  for(int i = 0; i < color_depth; i++){
+    for(int j = 0; j < i+1; j++){
+      transfer_function[i] += color_depth*((float)histogram[j])/(image_size);
     }
-    int n_threads = atoi(argv[2]);
-
-    unsigned char* image = read_bmp(argv[1]);
-    unsigned char* output_image = malloc(sizeof(unsigned char) * image_size);
+  }
 
 
-    int* histogram = (int*)calloc(sizeof(int), color_depth);
-    #pragma omp parallel for
-    for(int i = 0; i < image_size; i++){
-        histogram[image[i]]++;
+  for(int i = 0; i < image_size; i++){
+    output_image[i] = transfer_function[image[i]];
+  }
+
+  write_bmp(output_image, image_width, image_height);
+
+  int no_errors = 1;
+  for(int i=0;i<image_size;i++) {
+    if(!(output_image[i] == fasit[i]
+        || output_image[i]+1 == fasit[i]
+        || output_image[i]-1 == fasit[i])) {
+      no_errors = 0;
     }
-
-
-    float* transfer_function = (float*)calloc(sizeof(float), color_depth);
-    #pragma omp parallel for
-    for(int i = 0; i < color_depth; i++){
-        for(int j = 0; j < i+1; j++){
-            transfer_function[i] += color_depth*((float)histogram[j])/(image_size);
-        }
-    }
-
-
-    for(int i = 0; i < image_size; i++){
-        output_image[i] = transfer_function[image[i]];
-    }
-
-    write_bmp(output_image, image_width, image_height);
+  }
+  printf("Correct: %d\n", no_errors);
 }
